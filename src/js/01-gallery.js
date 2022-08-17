c307998f0e040927dcad55380dac1307bbbd15e6
YevheniiProshkin <evgeniyprofi@ukr.net>
2022-08-17 16:21:52 +0300

    Task 1 Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
+galleryEl.innerHTML = galleryMarkup;
+
+const slide = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li><a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" 
+          alt="${description}" />
+        </a></li>`
+    )
+    .join('');
+}
 console.log(galleryItems);
