d3bed61ee6067a0d3120fb54312513c26f5a683d
YevheniiProshkin <evgeniyprofi@ukr.net>
2022-08-18 13:39:22 +0300

    Redoned Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
